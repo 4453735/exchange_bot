e69e311682f3916b669513af9598c2d17d6bf0ad
4453735 <4453735@gmail.com>
2023-01-24 13:03:28 +0300

    Add dollar and turkish lira

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,3 +1,5 @@
 import 'dotenv/config';
 
 export const BOT_TOKEN = process.env.BOT_TOKEN!;
+export const CURRENCY_TOKEN = process.env.CURRENCY_TOKEN!;
+export const CURRENCY_TOKEN2 = process.env.CURRENCY_TOKEN2!;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 import { Telegraf } from 'telegraf';
 import {BOT_TOKEN} from "./AppConfig";
-
-// require("dotenv").config();
-
-// const app = require('express')();
-
-
-// console.log(process.env.BOT_TOKEN);
+import {getDollar, getLira} from "./api";
 
 
 const bot = new Telegraf(BOT_TOKEN);
@@ -32,10 +26,39 @@ bot.command('getUsers', async (ctx) => {
 })
 
 bot.command('getDollar', async (ctx) => {
-    const response = axios.get('https://jsonplaceholder.typicode.com/users');
-    const firstUser = (await response).data[0];
-    console.table((await response).data[0]);
-    ctx.reply(firstUser.name);
-})
+    const response = await getDollar();
+    setInterval(async () => {
+        const response = await getDollar();
+        let responseOld = response;
+
+        if (response != responseOld) {
+            ctx.reply(`${response}`);
+        }
+    }, 18000000);
+    ctx.reply(`${response}`);
+});
+
+bot.command('getLira', async (ctx) => {
+    const response = await getLira();
+    let responseOld = response;
+    setInterval(async () => {
+        const response = await getLira();
+
+        if (response > responseOld) {
+            ctx.reply(`Курс Турецкой лиры подрос! Новый курс: ${response} RUB`);
+            responseOld = response;
+        } else if (response < responseOld) {
+            ctx.reply(`Курс Турецкой лиры упал! Новый курс: ${response} RUB`);
+            responseOld = response;
+        }
+    }, 18000000);
+    ctx.reply(`Курс Турецкой лиры: ${response} RUB`);
+});
+
+// bot.command('getDollar2', async (ctx) => {
+//     await getDollar2();
+//
+//     // ctx.reply()
+// })
 
 bot.launch();
